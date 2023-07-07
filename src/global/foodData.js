1592fb9072c15f6e5559aa9b799b5f33b70b2b61
lukpaikat <mhasbisaputra5@gmail.com>
2023-07-08 07:12:12 +0800

    add foods data

---
+++ src/global/foodData.js
@@ -0,0 +1,37 @@
+export const foodCookTimes = {
+  fish: {
+    cooked: 40,
+    burnt: 80,
+  },
+  trophyFish: {
+    cooked: 90,
+    burnt: 180,
+  },
+  meat: {
+    cooked: 60,
+    burnt: 120,
+  },
+  beastMeat: {
+    cooked: 120,
+    burnt: 240,
+  },
+};
+
+export const foodDescription = {
+  fish: {
+    label: "Fish",
+    description: "Regular-sized fish",
+  },
+  trophyFish: {
+    label: "Trophy Fish",
+    description: "Larger fish",
+  },
+  meat: {
+    label: "Meat",
+    description: "Chicken, pork, snake & Shark"
+  },
+  beastMeat: {
+    label: "Beast Meat",
+    description: "Kraken & Megalodon"
+  }
+};
