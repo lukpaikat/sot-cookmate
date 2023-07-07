7e3eaca6209f3cdba35be5f894965621f52fba62
lukpaikat <mhasbisaputra5@gmail.com>
2023-07-07 18:05:36 +0800

    add: visual alarm animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,10 +28,26 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        "horizontal-pulse": {
+          "0%": {
+            opacity: "0",
+            transform: "scaleY(0)",
+            transformOrigin: "center",
+          },
+          "50%": {
+            opacity: "1",
+            transform: "scaleY(1)",
+          },
+          "100%": {
+            opacity: "0",
+            transform: "scaleY(0)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "horizontal-pulse": "horizontal-pulse 0.5s ease-in-out infinite",
       },
       fontFamily: {
         "josefin-sans": ["Josefin Sans", ...defaultTheme.fontFamily.sans],
