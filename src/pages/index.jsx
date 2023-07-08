b232808f260af4a6d0b6ad2f43556d9237084546
lukpaikat <mhasbisaputra5@gmail.com>
2023-07-08 10:04:16 +0800

    feat: sound alarm

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,22 +2,29 @@ import * as React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import { Button } from "@/components/ui/button";
-import {
-  Stop,
-  ArrowCounterClockwise,
-} from "@phosphor-icons/react";
+import { Stop, ArrowCounterClockwise } from "@phosphor-icons/react";
 import { useTimer } from "react-timer-hook";
+import useSound from "use-sound";
 import formatSeconds from "../lib/formatSeconds";
 import limitSeconds from "../lib/limitSeconds";
-import visualAlarmBackgroundColor from '../global/visualAlarmBackground';
-import { foodCookTimes, foodDescription } from '../global/foodData';
-import FoodDialog from '../components/ui/foodDialog';
+import visualAlarmBackgroundColor from "../global/visualAlarmBackground";
+import { foodCookTimes, foodDescription } from "../global/foodData";
+import FoodDialog from "../components/ui/foodDialog";
+import cookedSound from "../sounds/cooked-sound.mp3";
+import burntSound from "../sounds/burnt-sound.mp3";
+import onFireSound from "../sounds/on-fire-sound.mp3";
 
 const IndexPage = () => {
+  const [audioContext, setAudioContext] = React.useState(null);
   const [foodStatus, setFoodStatus] = React.useState("raw"); // raw, cooked, burnt, onFire
   const [isStarted, setIsStarted] = React.useState(false);
   const [selectedFood, setSelectedFood] = React.useState("meat"); // fish, trophyFish, meat, beastMeat
-  
+
+  /* Sound hooks */
+  const [playCooked] = useSound(cookedSound);
+  const [playBurnt] = useSound(burntSound);
+  const [playOnFire] = useSound(onFireSound);
+
   const timeToCooked = foodCookTimes[selectedFood].cooked;
   const timeToBurnt = foodCookTimes[selectedFood].burnt;
   const timeToOnFire = 300;
@@ -32,7 +39,10 @@ const IndexPage = () => {
   const { start, pause, resume, restart, totalSeconds, isRunning } = useTimer({
     expiryTimestamp: getMaxCookingTime,
     autoStart: false,
-    onExpire: () => setFoodStatus("onFire"),
+    onExpire: () => {
+      setFoodStatus("onFire");
+      playOnFire();
+    },
   });
 
   const timerStart = () => {
@@ -42,6 +52,9 @@ const IndexPage = () => {
     if (isStarted) {
       resume();
       return;
+    }
+    if (audioContext === null) {
+      initializeAudioContext();
     }
     start();
     setIsStarted(true);
@@ -74,14 +87,36 @@ const IndexPage = () => {
     limit: timeToBurnt,
   });
 
-  React.useLayoutEffect(() => {
+  const initializeAudioContext = () => {
+    // Check if the window object is available
+    if (typeof window === 'undefined' || !window.AudioContext) {
+      return;
+    }
+
+    // Create a new AudioContext instance
+    const context = new (window.AudioContext || window.webkitAudioContext)();
+
+    // Play a silent sound to activate the audio context
+    const buffer = context.createBuffer(1, 1, context.sampleRate);
+    const source = context.createBufferSource();
+    source.buffer = buffer;
+    source.connect(context.destination);
+    source.start();
+
+    // Set the initialized audio context to the state
+    setAudioContext(context)
+  }
+
+  React.useEffect(() => {
     if (foodStatus === "raw" && toCookedSeconds === 0) {
       setFoodStatus("cooked");
+      playCooked();
     }
     if (foodStatus === "cooked" && toBurntSeconds === 0) {
       setFoodStatus("burnt");
+      playBurnt();
     }
-  }, [foodStatus, toBurntSeconds, toCookedSeconds]);
+  }, [foodStatus, toBurntSeconds, toCookedSeconds, playBurnt, playCooked]);
 
   return (
     <Layout>
@@ -90,7 +125,9 @@ const IndexPage = () => {
         {/* Food Label */}
         <div className="flex items-start justify-between">
           <div>
-            <h1 className="text-3xl font-bold">{foodDescription[selectedFood].label}</h1>
+            <h1 className="text-3xl font-bold">
+              {foodDescription[selectedFood].label}
+            </h1>
             <p>{foodDescription[selectedFood].description}</p>
           </div>
           {/* <Button variant="ghost" size="icon">
@@ -100,7 +137,11 @@ const IndexPage = () => {
               className="text-stone-50"
             />
           </Button> */}
-          <FoodDialog onFoodSelected={setSelectedFood} currentFood={selectedFood} stopTimer={timerStop} />
+          <FoodDialog
+            onFoodSelected={setSelectedFood}
+            currentFood={selectedFood}
+            stopTimer={timerStop}
+          />
         </div>
         {/* Cooked Time Display */}
         <div className="overflow-hidden">
